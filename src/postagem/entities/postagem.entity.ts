37c9dc9acea62c65ec2afe705572f1e0c497ca4d
diego1999dd <diego.9090dd@gmail.com>
2025-01-13 13:51:43 -0300

    Criação da pasta postagem, controllers, entities e services

---
+++ src/postagem/entities/postagem.entity.ts
@@ -0,0 +1,24 @@
+import { IsNotEmpty } from 'class-validator';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'tb_postagens' }) // CREATE TABLE tb_postagens()
+export class Postagem {
+  @PrimaryGeneratedColumn() // INT AUTO_INCREMENT PRIMARY KEY
+  id: number;
+
+  @IsNotEmpty() // Validação dos dados do objeto
+  @Column({ length: 100, nullable: false }) // VARCHAR(100) NOT FULL
+  titulo: string;
+
+  @IsNotEmpty() // Validação dos dados do objeto
+  @Column({ length: 1000, nullable: false }) // VARCHAR(1000) NOT FULL
+  texto: string;
+
+  @UpdateDateColumn()
+  data: Date;
+}
